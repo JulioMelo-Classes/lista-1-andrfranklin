4a5638f670f0e7ccac72f4cec41806b66e984595
andrfranklin <andrew.frank146@gmail.com>
2021-06-20 22:53:21 +0000

    ponto_em_retangulo_1 function done

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -8,6 +8,12 @@ location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
     // TODO: Coloque aqui seu código.
 
+		if((P.x > IE.x && P.x < SD.x) && (P.y > IE.y && P.y < SD.y)){
+			return location_t::INSIDE;
+		}else if((P.x == IE.x && P.y >= IE.y && P.y <= SD.y) || (P.x == SD.x && P.y >= IE.y && P.y <= SD.y) || (P.y == SD.y && P.x >= IE.x && P.x <= SD.x) || (P.y == IE.y && P.x >= IE.x && P.x <= SD.x)){
+			return location_t::BORDER;
+		}
+
     // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
     return location_t::OUTSIDE;
 }
